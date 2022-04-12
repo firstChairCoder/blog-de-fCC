e53dcdd9ee4563fcdc5f4173cbd05eaa722981cf
firstChairCoder <md1qq7@gmail.com>
2022-04-12 14:39:40 +0100

    Extra portfolio inputs.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import {
+    Container,
+    Box,
+    Heading,
+    Image,
+    useColorModeValue,
+    Button,
+    Link
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+
+import Section from "../components/section";
+import Paragraph from "../components/paragraph";
+import { BioSection, BioYear } from "../components/bio";
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+                p={3}
+                mb={6}
+                align="center"
+            >
                 Hello, I&apos;m a front-end developer from Nigeria
             </Box>
 
@@ -12,9 +32,91 @@ const Page = () => {
                     <Heading as="h2" variant="page-title">
                         Joshua A.
                     </Heading>
-                    <p>Javascript Developer  |  Resident code flâneur</p>
+                    <p>Javascript Developer | Resident code flâneur</p>
+                </Box>
+
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                >
+                    <Image
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        maxWidth="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        src="/images/coder.png"
+                        alt="Profile Image"
+                    />
                 </Box>
             </Box>
+
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <Paragraph>
+                    Joshua is a front-end developer who is fascinated with all
+                    that JavaScript has done and can do when enployed by the
+                    curious mind. When not working on real-life projects or
+                    assisting colleagues at work on corporate and personal
+                    projects, he is more likely than not, somewhere out there,
+                    with a good book in his hand... and a cup of tea closeby.
+                </Paragraph>
+
+                <Box align="center" my={4}>
+                    <NextLink href="/works" scroll={false}>
+                        <Button
+                            rightIcon={<ChevronRightIcon />}
+                            colorScheme="teal"
+                        >
+                            My portfolio
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Section>
+
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title">
+                    Brief Bio
+                </Heading>
+                <BioSection>
+                    <BioYear>1996</BioYear>
+                    Born in Abuja, Nigeria.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2018</BioYear>
+                    Completed my degree program in Electrical and Electronics
+                    Engineering from Covenant University.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2019-2020</BioYear>
+                    Worked in the private insurance sector while working
+                    freelance by the side
+                </BioSection>
+                <BioSection>
+                    <BioYear>2020-present</BioYear>
+                    Worked in various capacities with React Native Nigeria among
+                    others.
+                </BioSection>
+            </Section>
+
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title">
+                    I ♥
+                </Heading>
+
+                <Paragraph>
+                    Music, Basketball, {" "}
+                    <Link href="https://illust.odoruinu.net/" target="_blank">
+                        Armchair Psychology
+                    </Link>
+                    , Machine Learning, Reading
+                </Paragraph>
+            </Section>
         </Container>
     );
 };
