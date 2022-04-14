06750e6bd017212642e363fd5b0b22e9c2262531
firstChairCoder <md1qq7@gmail.com>
2022-04-14 09:47:11 +0100

    Vercel and other config.

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,20 @@ import {
     useColorModeValue,
     Button,
     Link,
+    SimpleGrid,
+    List,
+    ListItem,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { ChevronRightIcon } from "@chakra-ui/icons";
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from "react-icons/io5";
 
 import Section from "../components/section";
 import Paragraph from "../components/paragraph";
 import { BioSection, BioYear } from "../components/bio";
 import Layout from "../components/layouts/article";
+import { GridItem } from "../components/grid-item";
+import thumbYouTube from "../public/images/links/youtube.png";
 
 const Page = () => {
     return (
@@ -122,6 +128,63 @@ const Page = () => {
                         </Link>
                         , Machine Learning, Reading
                     </Paragraph>
+                </Section>
+
+                <Section delay={0.3}>
+                    <Heading as="h3" variant="section-title">
+                        Online presence
+                    </Heading>
+                    <List>
+                        <ListItem>
+                            <Link
+                                href="https://github.com/firstChairCoder"
+                                target="_blank"
+                            >
+                                <Button
+                                    variant="ghost"
+                                    colorScheme="teal"
+                                    leftIcon={<IoLogoGithub />}
+                                >
+                                    @firstChairCoder
+                                </Button>
+                            </Link>
+                        </ListItem>
+                        <ListItem>
+                            <Link
+                                href="https://twitter.com/firstChairCoder"
+                                target="_blank"
+                            >
+                                <Button
+                                    variant="ghost"
+                                    colorScheme="teal"
+                                    leftIcon={<IoLogoTwitter />}
+                                >
+                                    @firstChairCoder
+                                </Button>
+                            </Link>
+                        </ListItem>
+                    </List>
+
+                    <SimpleGrid columns={[1, 2, 2]} gap={6}>
+                        <GridItem
+                            href="https://www.youtube.com/devaslife"
+                            title="Dev as Life"
+                            thumbnail={thumbYouTube}
+                        >
+                            My mentor's YouTube channel
+                        </GridItem>
+                    </SimpleGrid>
+
+                    <Box align="center" my={4}>
+                        <NextLink href="/posts" scroll={false}>
+                            <Button
+                                rightIcon={<ChevronRightIcon />}
+                                colorScheme="teal"
+                            >
+                                Popular posts
+                            </Button>
+                        </NextLink>
+                    </Box>
                 </Section>
             </Container>
         </Layout>
